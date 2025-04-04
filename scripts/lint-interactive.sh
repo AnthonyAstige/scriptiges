5284641e050cbf101fe9bc14ee82ee442fbc4cce
Anthony Astige <Anthony@Astige.com>
2025-04-04 08:54:48 -0500

    Disable warning JSON modules

--- scripts/lint-interactive.sh
+++ scripts/lint-interactive.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Bandaid for error:
+# > (node:17827) ExperimentalWarning: Importing JSON modules is an experimental feature and might change at any time
+# - https://github.com/nodejs/node/issues/51347#issuecomment-1893074523
+# Hopefully we don't need this when node updates as JSON modules seem standard?
+# - https://github.com/nodejs/node/issues/51347#issuecomment-2476696604
+# - Anthony 2025-04-04
+export NODE_OPTIONS="--disable-warning=ExperimentalWarning"
+
 # Get the directory where this script is located.
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
