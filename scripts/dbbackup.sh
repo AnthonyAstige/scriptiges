b046742a0cb0734d1dabec8e6c7b0a21ebbb3110
Anthony Astige <Anthony@Astige.com>
2025-04-15 14:12:18 -0500

    Create backup dir correctly

--- scripts/dbbackup.sh
+++ scripts/dbbackup.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+# Function to find the project root (directory containing .git)
+find_project_root() {
+  local current_dir="$1"
+  while [ ! -d "$current_dir/.git" ]; do
+    if [ "$current_dir" = "/" ]; then
+      echo "Error: Not inside a git repository."
+      exit 1
+    fi
+    current_dir=$(dirname "$current_dir")
+  done
+  echo "$current_dir"
+}
+
+# Find the project root directory, starting from the current working directory
+PROJECT_ROOT=$(find_project_root "$(pwd)")
+echo "PROOT: $PROJECT_ROOT"
+
+# Define the backup directory relative to the project root
+BACKUP_DIR="${PROJECT_ROOT}/.data/postgres_dumps"
+echo "BACKUP_DIR: $BACKUP_DIR"
+
+# Check if the backup directory exists, and create it if it doesn't
+if [ ! -d "${BACKUP_DIR}" ]; then
+  mkdir -p "${BACKUP_DIR}"
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to create backup directory ${BACKUP_DIR}"
+    exit 1
+  fi
+fi
+
 # Check if environment variables are set
 if [ -z "${DOCKER_CONTAINER_FULLDBDUMPER_NAME}" ]; then
   echo "Error: DOCKER_CONTAINER_FULLDBDUMPER_NAME is not set"
@@ -30,4 +60,4 @@ if [ -z "${POSTGRES_PASSWORD}" ]; then
   exit 1
 fi
 
-docker-compose run --rm backuppostgres once
+# docker-compose -f "${SCRIPT_DIR}/../docker-compose.yml" run --rm backuppostgres once
